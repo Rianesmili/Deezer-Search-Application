8e97927064955751ae5dbd32e2c0fd535b1f7ad1
Riane SMILI <rianesmili@gmail.com>
2024-02-21 09:25:36 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "deezer_search_app"
+include(":app")
+ 
